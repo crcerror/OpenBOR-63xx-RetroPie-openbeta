815184cdf039378e21df796529f3060aaa820308
cyperghost <crcerror@users.noreply.github.com>
2018-10-05 14:52:21 +0200

    Corrected version number!

--- scriptmodules/openbor-6xxx.sh
+++ scriptmodules/openbor-6xxx.sh
@@ -10,7 +10,7 @@
 #
 
 rp_module_id="openbor-6xxx"
-rp_module_desc="OpenBOR - Beat 'em Up Game Engine v6512 (official!)"
+rp_module_desc="OpenBOR - Beat 'em Up Game Engine v6315 (official!)"
 rp_module_help="Place your pak files in $romdir/ports/openbor and then run $romdir/ports/OpenBOR.sh from ports section."
 rp_module_licence="BSD https://raw.githubusercontent.com/crcerror/OpenBOR-Raspberry/master/LICENSE"
 rp_module_section="exp"
